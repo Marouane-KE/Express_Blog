7767e289a6f56d532453d601f3cdb32e9f6b3c2a
Marouane-KE <kamale.elidrissi@gmail.com>
2023-09-13 12:25:30 +0100

    separate middleware

--- Public/js/authentication.js
+++ Public/js/authentication.js
@@ -1,11 +1,17 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
-
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
 });
-
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+  signupBtn.click();
+  return false;
 });
--- Routes/routers.js
+++ Routes/routers.js
@@ -1,44 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const multer = require("multer");
+
 const { createBlog,getBlogs } = require("../Controllers/blogController");
 const {userRegister } = require("../Controllers/usersController");
+const logger =require("../Middleware/logger")
+const upload =require("../Middleware/upload")
 const { render } = require("ejs");
 const jwt = require('jsonwebtoken')
-const secret="lmkawi";
+
 const cookies = require('cookie-parser')
 router.use(cookies());
 
 
-const logger =(req,res,next)=>{
-  const token = req.cookies.token_auth
-  if(!token){
-      return res.redirect('/authentication')
-  }
-  const decoded = jwt.verify(token,secret)
-  if(!decoded){
-       res.redirect('/authentication')
-  }
-  const {email,image } = decoded
-  req.email = email
-  req.image = image
-  next()
-}
 
 
 
-// Configure Multer
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./Public/uploads/"); // Store uploaded files in the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname); // Set a unique filename
-  },
-});
 
-const upload = multer({ storage: storage });
+
 
 
 router.get('/authentication',(req,res)=>{
@@ -59,7 +38,7 @@ router.get("/addBlog",logger, (req, res) => {
 });
 router.post("/createBlog", upload.single("image"), createBlog);
 
-router.get("/allBlogs",logger,getBlogs);
+router.get("/allBlogs",getBlogs);
 
 
 module.exports = router;
